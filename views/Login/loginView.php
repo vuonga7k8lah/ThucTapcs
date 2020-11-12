93ddbab70a379f2c28a9a3f720e785bb1fe73e4a
Vuong_DZ <vuonga7k8lnc@gmail.com>
2020-11-12 22:07:11 +0700

    create Login

--- views/Login/loginView.php
+++ views/Login/loginView.php
@@ -6,25 +6,31 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 login-sec">
-                <h2 class="text-center">Login Now</h2>
+                <h2 class="text-center">Đăng Nhập</h2>
                 <form class="login-form">
                     <div class="form-group">
-                        <label for="exampleInputEmail1" class="text-uppercase">Username</label>
-                        <input type="text" class="form-control" placeholder="">
+                        <label for="exampleInputEmail1" class="text-uppercase">Tài Khoản:</label>
+                        <input type="text" class="form-control" required >
 
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputPassword1" class="text-uppercase">Password</label>
-                        <input type="password" class="form-control" placeholder="">
+                        <label for="exampleInputPassword1" class="text-uppercase">Mật Khẩu:</label>
+                        <input type="password" class="form-control" required >
                     </div>
-
-
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Truy Cập Với Tư Cách:</label>
+                        <select class="form-control" id="exampleFormControlSelect1" name="access">
+                            <option value="sinhvien">Sinh Viên</option>
+                            <option value="giangvien">Giảng Viên</option>
+                            <option value="admin">Admin</option>
+                        </select>
+                    </div>
                     <div class="form-check">
                         <label class="form-check-label">
                             <input type="checkbox" class="form-check-input">
                             <small>Remember Me</small>
                         </label>
-                        <button type="submit" class="btn btn-login float-right">Submit</button>
+                        <button type="submit" class="btn btn-login float-right">Gửi</button>
                     </div>
 
                 </form>
